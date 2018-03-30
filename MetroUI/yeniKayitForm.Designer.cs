81b57a8f00d5807d5b0c4368f3936354ac833895
Ender İMEN <enderimen@hotmail.com>
2018-03-31 00:16:40 +0300

    New Member Enrollment Page.

---
+++ MetroUI/yeniKayitForm.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace MetroUI
+{
+  partial class yeniKayitForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(yeniKayitForm));
+      this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+      this.textBox5 = new System.Windows.Forms.TextBox();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label9 = new System.Windows.Forms.Label();
+      this.textBox6 = new System.Windows.Forms.TextBox();
+      this.textBox7 = new System.Windows.Forms.TextBox();
+      this.label10 = new System.Windows.Forms.Label();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.btnKaydet = new System.Windows.Forms.Button();
+      this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+      this.btnGozat = new System.Windows.Forms.Button();
+      this.btnGuncelle = new System.Windows.Forms.Button();
+      this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+      this.label7 = new System.Windows.Forms.Label();
+      this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+      this.label8 = new System.Windows.Forms.Label();
+      this.label11 = new System.Windows.Forms.Label();
+      this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // maskedTextBox1
+      // 
+      this.maskedTextBox1.Location = new System.Drawing.Point(94, 242);
+      this.maskedTextBox1.Mask = "0(000)000 00 00 ";
+      this.maskedTextBox1.Name = "maskedTextBox1";
+      this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+      this.maskedTextBox1.TabIndex = 5;
+      this.maskedTextBox1.Text = "0";
+      // 
+      // textBox5
+      // 
+      this.textBox5.Location = new System.Drawing.Point(94, 189);
+      this.textBox5.Multiline = true;
+      this.textBox5.Name = "textBox5";
+      this.textBox5.Size = new System.Drawing.Size(129, 46);
+      this.textBox5.TabIndex = 4;
+      this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(94, 162);
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(100, 20);
+      this.textBox4.TabIndex = 3;
+      this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(94, 108);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(100, 20);
+      this.textBox2.TabIndex = 1;
+      this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(94, 80);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 0;
+      this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(19, 196);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(37, 13);
+      this.label6.TabIndex = 5;
+      this.label6.Text = "Adres:";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(19, 246);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(46, 13);
+      this.label4.TabIndex = 6;
+      this.label4.Text = "Telefon:";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(19, 165);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(53, 13);
+      this.label5.TabIndex = 7;
+      this.label5.Text = "*Okul No:";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(19, 135);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(36, 13);
+      this.label3.TabIndex = 8;
+      this.label3.Text = "*Sınıfı:";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(19, 111);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(46, 13);
+      this.label2.TabIndex = 9;
+      this.label2.Text = "*Soyadı:";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(19, 82);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(29, 13);
+      this.label1.TabIndex = 10;
+      this.label1.Text = "*Adı:";
+      // 
+      // label9
+      // 
+      this.label9.AutoSize = true;
+      this.label9.Location = new System.Drawing.Point(19, 275);
+      this.label9.Name = "label9";
+      this.label9.Size = new System.Drawing.Size(64, 13);
+      this.label9.TabIndex = 6;
+      this.label9.Text = "Kayıt Ücreti:";
+      // 
+      // textBox6
+      // 
+      this.textBox6.Location = new System.Drawing.Point(94, 270);
+      this.textBox6.Name = "textBox6";
+      this.textBox6.Size = new System.Drawing.Size(100, 20);
+      this.textBox6.TabIndex = 6;
+      this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
+      // 
+      // textBox7
+      // 
+      this.textBox7.Location = new System.Drawing.Point(94, 298);
+      this.textBox7.Name = "textBox7";
+      this.textBox7.Size = new System.Drawing.Size(100, 20);
+      this.textBox7.TabIndex = 7;
+      this.textBox7.Leave += new System.EventHandler(this.textBox7_Leave);
+      // 
+      // label10
+      // 
+      this.label10.AutoSize = true;
+      this.label10.Location = new System.Drawing.Point(19, 301);
+      this.label10.Name = "label10";
+      this.label10.Size = new System.Drawing.Size(39, 13);
+      this.label10.TabIndex = 18;
+      this.label10.Text = "Görev:";
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(229, 80);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(119, 121);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // btnKaydet
+      // 
+      this.btnKaydet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+      this.btnKaydet.FlatAppearance.BorderSize = 0;
+      this.btnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnKaydet.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.btnKaydet.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+      this.btnKaydet.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnKaydet.ImageKey = "save.png";
+      this.btnKaydet.ImageList = this.ımageList1;
+      this.btnKaydet.Location = new System.Drawing.Point(232, 378);
+      this.btnKaydet.Name = "btnKaydet";
+      this.btnKaydet.Size = new System.Drawing.Size(116, 37);
+      this.btnKaydet.TabIndex = 10;
+      this.btnKaydet.Text = "             Kaydet";
+      this.btnKaydet.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnKaydet.UseVisualStyleBackColor = false;
+      this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+      // 
+      // ımageList1
+      // 
+      this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+      this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+      this.ımageList1.Images.SetKeyName(0, "save.png");
+      // 
+      // btnGozat
+      // 
+      this.btnGozat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+      this.btnGozat.FlatAppearance.BorderSize = 0;
+      this.btnGozat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnGozat.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.btnGozat.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+      this.btnGozat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnGozat.ImageKey = "save.png";
+      this.btnGozat.Location = new System.Drawing.Point(317, 207);
+      this.btnGozat.Name = "btnGozat";
+      this.btnGozat.Size = new System.Drawing.Size(31, 28);
+      this.btnGozat.TabIndex = 9;
+      this.btnGozat.Text = "...";
+      this.toolTip1.SetToolTip(this.btnGozat, "Gözat");
+      this.btnGozat.UseVisualStyleBackColor = false;
+      this.btnGozat.Click += new System.EventHandler(this.btnGozat_Click);
+      // 
+      // btnGuncelle
+      // 
+      this.btnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+      this.btnGuncelle.FlatAppearance.BorderSize = 0;
+      this.btnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnGuncelle.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.btnGuncelle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+      this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnGuncelle.ImageKey = "edit.png";
+      this.btnGuncelle.ImageList = this.ımageList2;
+      this.btnGuncelle.Location = new System.Drawing.Point(99, 378);
+      this.btnGuncelle.Name = "btnGuncelle";
+      this.btnGuncelle.Size = new System.Drawing.Size(127, 37);
+      this.btnGuncelle.TabIndex = 11;
+      this.btnGuncelle.Text = "         Güncelle";
+      this.btnGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnGuncelle.UseVisualStyleBackColor = false;
+      this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click_1);
+      // 
+      // ımageList2
+      // 
+      this.ımageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList2.ImageStream")));
+      this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+      this.ımageList2.Images.SetKeyName(0, "edit.png");
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(19, 332);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(49, 13);
+      this.label7.TabIndex = 22;
+      this.label7.Text = "Kayıt Yılı:";
+      // 
+      // dateTimePicker1
+      // 
+      this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+      this.dateTimePicker1.Location = new System.Drawing.Point(94, 326);
+      this.dateTimePicker1.Name = "dateTimePicker1";
+      this.dateTimePicker1.Size = new System.Drawing.Size(100, 20);
+      this.dateTimePicker1.TabIndex = 8;
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Items.AddRange(new object[] {
+            "Hazırlık",
+            "1.Sınıf",
+            "2.Sınıf",
+            "3.Sınıf",
+            "4.Sınıf"});
+      this.comboBox1.Location = new System.Drawing.Point(94, 133);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(100, 21);
+      this.comboBox1.TabIndex = 2;
+      // 
+      // notifyIcon1
+      // 
+      this.notifyIcon1.Text = "notifyIcon1";
+      this.notifyIcon1.Visible = true;
+      // 
+      // label8
+      // 
+      this.label8.AutoSize = true;
+      this.label8.Location = new System.Drawing.Point(293, 220);
+      this.label8.Name = "label8";
+      this.label8.Size = new System.Drawing.Size(20, 13);
+      this.label8.TabIndex = 27;
+      this.label8.Text = "yol";
+      this.label8.Visible = false;
+      // 
+      // label11
+      // 
+      this.label11.AutoSize = true;
+      this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F);
+      this.label11.Location = new System.Drawing.Point(216, 80);
+      this.label11.Name = "label11";
+      this.label11.Size = new System.Drawing.Size(12, 15);
+      this.label11.TabIndex = 28;
+      this.label11.Text = "*";
+      // 
+      // toolTip1
+      // 
+      this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+      // 
+      // yeniKayitForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+      this.ClientSize = new System.Drawing.Size(362, 433);
+      this.Controls.Add(this.label11);
+      this.Controls.Add(this.label8);
+      this.Controls.Add(this.comboBox1);
+      this.Controls.Add(this.dateTimePicker1);
+      this.Controls.Add(this.label7);
+      this.Controls.Add(this.btnGuncelle);
+      this.Controls.Add(this.btnGozat);
+      this.Controls.Add(this.btnKaydet);
+      this.Controls.Add(this.textBox7);
+      this.Controls.Add(this.label10);
+      this.Controls.Add(this.textBox6);
+      this.Controls.Add(this.maskedTextBox1);
+      this.Controls.Add(this.textBox5);
+      this.Controls.Add(this.textBox4);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label9);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.pictureBox1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.Name = "yeniKayitForm";
+      this.Resizable = false;
+      this.Style = MetroFramework.MetroColorStyle.Orange;
+      this.Text = "Yeni Üye";
+      this.Load += new System.EventHandler(this.yeniKayitForm_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    private System.Windows.Forms.TextBox textBox5;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label9;
+    private System.Windows.Forms.TextBox textBox6;
+    private System.Windows.Forms.TextBox textBox7;
+    private System.Windows.Forms.Label label10;
+    private System.Windows.Forms.Button btnKaydet;
+    private System.Windows.Forms.ImageList ımageList1;
+    private System.Windows.Forms.Button btnGozat;
+    private System.Windows.Forms.Button btnGuncelle;
+    private System.Windows.Forms.ImageList ımageList2;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.NotifyIcon notifyIcon1;
+    private System.Windows.Forms.Label label11;
+    public System.Windows.Forms.Label label8;
+    private System.Windows.Forms.ToolTip toolTip1;
+  }
+}
---
+++ MetroUI/yeniKayitForm.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.IO;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+
+namespace MetroUI
+{
+  public partial class yeniKayitForm : MetroForm
+  {
+      private int _ID,islem;
+    public yeniKayitForm(int pr1,int islem)
+    {  
+        if (pr1 == 0 && islem == 0)
+          {
+            System.Windows.Forms.MessageBox.Show("Lütfen kayıt seçiniz. " + pr1);
+              //this.Close();
+          }     
+              this.islem=islem;
+              InitializeComponent();
+              this._ID = pr1;
+    }
+    SqlConnection connection = new SqlConnection("server=ACER-E1-571G\\SQLEXPRESS01;Database=KlupOtomasyon;Integrated Security=true");
+  
+    private void btnKaydet_Click(object sender, EventArgs e)
+    {
+      if (textBox1.Text == "" || textBox2.Text == "" || textBox4.Text == "" || comboBox1.Text == "" || label8.Text == "")
+      {
+        System.Windows.Forms.MessageBox.Show("LÜTFEN YILDIZLI ALANLARI DOLDURUNUZ");
+      }
+      else
+      {
+        connection.Open();
+
+        SqlCommand cekle = new SqlCommand("insert into Uyeler (uye_ad,uye_soyad,uye_sinif,uye_no,uye_adres,uye_telefon,uye_kayitucreti,uye_gorev,uye_kayityil,uye_resim) values (@prmtr2,@prmtr3,@prmtr4,@prmtr5,@prmtr6,@prmtr7,@prmtr8,@prmtr9,@prmtr10,@prmtr11)", connection);
+
+        cekle.Parameters.AddWithValue("@prmtr2", textBox1.Text);
+        cekle.Parameters.AddWithValue("@prmtr3", textBox2.Text);
+        cekle.Parameters.AddWithValue("@prmtr4", comboBox1.Text);
+        cekle.Parameters.AddWithValue("@prmtr5", textBox4.Text);
+        cekle.Parameters.AddWithValue("@prmtr6", textBox5.Text);
+        cekle.Parameters.AddWithValue("@prmtr7", maskedTextBox1.Text);
+        cekle.Parameters.AddWithValue("@prmtr8", textBox6.Text);
+        cekle.Parameters.AddWithValue("@prmtr9", textBox7.Text);
+        cekle.Parameters.AddWithValue("@prmtr10", dateTimePicker1.Text);
+        cekle.Parameters.AddWithValue("@prmtr11", label8.Text);
+        cekle.ExecuteNonQuery();
+
+        connection.Close();
+        MessageBox.Show("KAYIT BAŞARILI");
+        this.Close();
+        anasayfa a = Application.OpenForms["anasayfa"] as anasayfa;
+        a.uyelerListele();
+      }
+    }
+
+    private void btnGozat_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog dosya = new OpenFileDialog();
+      dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png |  Tüm Dosyalar |*.*";
+      dosya.ShowDialog();
+      string dosyayolu = dosya.FileName;
+      label8.Text = dosyayolu;
+      pictureBox1.ImageLocation = dosyayolu;
+    }
+
+      private void yeniKayitForm_Load(object sender, EventArgs e)
+      {
+          label8.Text="C:\\DATA\\proje\\proje\\MetroUI\\MetroUI\\MetroUI\\bin\\Debug\\img\\userlogo.png";
+          if(islem==0)
+          {
+               btnKaydet.Visible = false;
+               this.Text="Güncelle";
+                connection.Open();
+
+                SqlCommand bul = new SqlCommand("select * FROM Uyeler WHERE uye_id=@prmtr1", connection);
+                bul.Parameters.AddWithValue("@prmtr1", _ID);
+
+
+                SqlDataReader drEd = bul.ExecuteReader();
+                if (drEd.Read())
+                {
+                    textBox1.Text = drEd["uye_ad"].ToString();
+                    textBox2.Text = drEd["uye_soyad"].ToString();
+                    comboBox1.Text = drEd["uye_sinif"].ToString();
+                    textBox4.Text = drEd["uye_no"].ToString();
+                    textBox5.Text = drEd["uye_adres"].ToString();
+                    maskedTextBox1.Text = drEd["uye_telefon"].ToString();
+                    textBox6.Text = drEd["uye_kayitucreti"].ToString();
+                    textBox7.Text = drEd["uye_gorev"].ToString();
+                    dateTimePicker1.Text = drEd["uye_kayityil"].ToString();
+                    pictureBox1.ImageLocation=drEd["uye_resim"].ToString();
+                }
+                connection.Close();
+          }
+          else
+          {
+              btnGuncelle.Visible = false;
+          }
+      
+  
+    }
+
+      private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+      {
+          char ch = e.KeyChar;
+          if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+          {
+              e.Handled = true;
+          }
+      }
+
+      private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+      {
+          char ch = e.KeyChar;
+          if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+          {
+              e.Handled = true;
+          }
+      }
+
+      private void btnGuncelle_Click_1(object sender, EventArgs e)
+      {
+        connection.Open();
+        SqlCommand guncelle = new SqlCommand("update Uyeler set uye_ad='" + textBox1.Text + "',uye_soyad='" + textBox2.Text + "',uye_sinif='" + comboBox1.Text + "',uye_no='" + textBox4.Text + "',uye_adres='" + textBox5.Text + "',uye_telefon='" + maskedTextBox1.Text + "',uye_kayitucreti='" + textBox6.Text + "',uye_gorev='" + textBox7.Text + "',uye_resim='" + label8.Text + "'  where uye_id='" + _ID + "'", connection);
+
+
+        guncelle.ExecuteNonQuery();
+        connection.Close();
+        MessageBox.Show("Kayıt Değiştirildi");
+        this.Close();
+        anasayfa a = Application.OpenForms["anasayfa"] as anasayfa;
+        a.uyelerListele();
+        a.kadroListele();
+      }
+      #region Büyük Harf/Küçük Harf
+      private void textBox2_Leave(object sender, EventArgs e)
+      {
+        textBox2.Text = textBox2.Text.ToUpper();
+      }
+
+      private void textBox7_Leave(object sender, EventArgs e)
+      {
+        textBox7.Text = textBox7.Text.ToUpper();
+
+      }
+
+      private void textBox1_Leave(object sender, EventArgs e)
+      {
+        textBox1.Text=CultureInfo.CurrentCulture.TextInfo.ToTitleCase(textBox1.Text);
+      }
+
+      private void textBox5_Leave(object sender, EventArgs e)
+      {
+        textBox5.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(textBox5.Text);
+      }
+      #endregion
+  }
+}
