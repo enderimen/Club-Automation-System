35b399631ca46b3e892b346b535c0c9bcebae22e
Ender İMEN <enderimen@hotmail.com>
2018-03-31 00:15:11 +0300

    Password Settings Page.

---
+++ MetroUI/sifreIslemleriForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MetroUI
+{
+  partial class sifreIslemleriForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sifreIslemleriForm));
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.checkBox3 = new System.Windows.Forms.CheckBox();
+      this.checkBox2 = new System.Windows.Forms.CheckBox();
+      this.tyeniSifre = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.yeniSifre = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.username = new System.Windows.Forms.TextBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+      this.panel1.Controls.Add(this.checkBox3);
+      this.panel1.Controls.Add(this.checkBox2);
+      this.panel1.Controls.Add(this.tyeniSifre);
+      this.panel1.Controls.Add(this.label4);
+      this.panel1.Controls.Add(this.yeniSifre);
+      this.panel1.Controls.Add(this.label3);
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Controls.Add(this.button1);
+      this.panel1.Controls.Add(this.username);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel1.Location = new System.Drawing.Point(0, 138);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(366, 308);
+      this.panel1.TabIndex = 6;
+      // 
+      // checkBox3
+      // 
+      this.checkBox3.AutoSize = true;
+      this.checkBox3.BackColor = System.Drawing.Color.White;
+      this.checkBox3.Location = new System.Drawing.Point(287, 150);
+      this.checkBox3.Name = "checkBox3";
+      this.checkBox3.Size = new System.Drawing.Size(15, 14);
+      this.checkBox3.TabIndex = 5;
+      this.checkBox3.UseVisualStyleBackColor = false;
+      this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+      // 
+      // checkBox2
+      // 
+      this.checkBox2.AutoSize = true;
+      this.checkBox2.BackColor = System.Drawing.Color.White;
+      this.checkBox2.Location = new System.Drawing.Point(287, 95);
+      this.checkBox2.Name = "checkBox2";
+      this.checkBox2.Size = new System.Drawing.Size(15, 14);
+      this.checkBox2.TabIndex = 4;
+      this.checkBox2.UseVisualStyleBackColor = false;
+      this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+      // 
+      // tyeniSifre
+      // 
+      this.tyeniSifre.Font = new System.Drawing.Font("Myriad Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.tyeniSifre.ForeColor = System.Drawing.Color.SlateGray;
+      this.tyeniSifre.Location = new System.Drawing.Point(58, 139);
+      this.tyeniSifre.Multiline = true;
+      this.tyeniSifre.Name = "tyeniSifre";
+      this.tyeniSifre.PasswordChar = '⚫';
+      this.tyeniSifre.Size = new System.Drawing.Size(246, 35);
+      this.tyeniSifre.TabIndex = 2;
+      this.tyeniSifre.Text = "1";
+      this.tyeniSifre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      this.tyeniSifre.WordWrap = false;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.label4.ForeColor = System.Drawing.Color.White;
+      this.label4.Location = new System.Drawing.Point(55, 120);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(107, 17);
+      this.label4.TabIndex = 5;
+      this.label4.Text = "Yeni Şifre Tekrarı";
+      // 
+      // yeniSifre
+      // 
+      this.yeniSifre.Font = new System.Drawing.Font("Myriad Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.yeniSifre.ForeColor = System.Drawing.Color.SlateGray;
+      this.yeniSifre.Location = new System.Drawing.Point(58, 84);
+      this.yeniSifre.Multiline = true;
+      this.yeniSifre.Name = "yeniSifre";
+      this.yeniSifre.PasswordChar = '⚫';
+      this.yeniSifre.Size = new System.Drawing.Size(246, 35);
+      this.yeniSifre.TabIndex = 1;
+      this.yeniSifre.Text = "1";
+      this.yeniSifre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      this.yeniSifre.WordWrap = false;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.label3.ForeColor = System.Drawing.Color.White;
+      this.label3.Location = new System.Drawing.Point(55, 66);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(63, 17);
+      this.label3.TabIndex = 3;
+      this.label3.Text = "Yeni Şifre";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.label1.ForeColor = System.Drawing.Color.White;
+      this.label1.Location = new System.Drawing.Point(55, 10);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(86, 17);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Kullanıcı Adı";
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+      this.button1.FlatAppearance.BorderSize = 0;
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.button1.Location = new System.Drawing.Point(58, 188);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(246, 39);
+      this.button1.TabIndex = 3;
+      this.button1.Text = "Değiştir";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // username
+      // 
+      this.username.Enabled = false;
+      this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+      this.username.ForeColor = System.Drawing.Color.SlateGray;
+      this.username.Location = new System.Drawing.Point(58, 30);
+      this.username.Multiline = true;
+      this.username.Name = "username";
+      this.username.Size = new System.Drawing.Size(246, 35);
+      this.username.TabIndex = 0;
+      this.username.Text = "Admin";
+      this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      this.username.WordWrap = false;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(128, 21);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(111, 111);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 5;
+      this.pictureBox1.TabStop = false;
+      // 
+      // sifreIslemleriForm
+      // 
+      this.AcceptButton = this.button1;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+      this.ClientSize = new System.Drawing.Size(366, 446);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.pictureBox1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.Name = "sifreIslemleriForm";
+      this.Padding = new System.Windows.Forms.Padding(0, 60, 0, 0);
+      this.Resizable = false;
+      this.Style = MetroFramework.MetroColorStyle.Orange;
+      this.Load += new System.EventHandler(this.sifreIslemleriForm_Load);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox username;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox tyeniSifre;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox yeniSifre;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.CheckBox checkBox3;
+    private System.Windows.Forms.CheckBox checkBox2;
+  }
+}
---
+++ MetroUI/sifreIslemleriForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace MetroUI
+{
+  public partial class sifreIslemleriForm : MetroForm
+  {
+    public sifreIslemleriForm()
+    {
+      InitializeComponent();
+    }
+    SqlConnection connection = new SqlConnection("server=ACER-E1-571G\\SQLEXPRESS01;Database=KlupOtomasyon;Integrated Security=true;MultipleActiveResultSets=True");
+    #region şifre göster işareti
+    
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+      
+    }
+
+    private void checkBox2_CheckedChanged(object sender, EventArgs e)
+    {
+      if (checkBox2.Checked)
+      {
+        //karakteri göster.
+        yeniSifre.PasswordChar = '\0';
+      }
+      //değilse karakterlerin yerine • koy.
+      else
+      {
+        yeniSifre.PasswordChar = '⚫';
+      }
+    }
+
+    private void checkBox3_CheckedChanged(object sender, EventArgs e)
+    {
+      if (checkBox3.Checked)
+      {
+        //karakteri göster.
+        tyeniSifre.PasswordChar = '\0';
+      }
+      //değilse karakterlerin yerine • koy.
+      else
+      {
+        tyeniSifre.PasswordChar = '⚫';
+      }
+    }
+    #endregion
+    
+    private void button1_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        if (connection.State != ConnectionState.Open)
+        {
+          connection.Open();   
+        }
+        SqlCommand sorgu = new SqlCommand("SELECT * FROM login order by kSifre", connection);
+        SqlDataReader datare = sorgu.ExecuteReader();
+        
+        while (datare.Read())
+        {
+          string a = Convert.ToString(username.Text);
+          string b = Convert.ToString(yeniSifre.Text);
+          string c = Convert.ToString(tyeniSifre.Text);
+
+
+          if (a == datare[1].ToString() && b==c)
+          {
+
+            SqlCommand komut = new SqlCommand("UPDATE login SET kSifre=" + yeniSifre.Text + " WHERE ID=1", connection);
+            int etkilenen=komut.ExecuteNonQuery();
+           
+            if (etkilenen>0)
+            {
+              MessageBox.Show("Şifreniz Değiştirildi", "BİLGİ"); this.Close(); 
+            }
+            
+            yeniSifre.Text = "";
+            tyeniSifre.Text = "";
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+          }
+          else
+          {
+            MessageBox.Show("Kullanıcı Adı/Şifre Hatalı", "HATA");
+            yeniSifre.Text = "";
+            tyeniSifre.Text = "";
+          }
+          
+        }
+        connection.Close();
+      }
+      catch (Exception hata)
+      {
+            MessageBox.Show(hata.Message);
+      } 
+    }
+
+    private void sifreIslemleriForm_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
