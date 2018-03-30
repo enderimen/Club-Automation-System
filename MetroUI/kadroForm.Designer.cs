c831dd9688d8ddcc5e3a9d7a37252b77d51379c3
Ender İMEN <enderimen@hotmail.com>
2018-03-31 00:09:02 +0300

    Management Team Page.

---
+++ MetroUI/kadroForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace MetroUI
+{
+  partial class Kulüp_Yönetimi
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kulüp_Yönetimi));
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.textBox5 = new System.Windows.Forms.TextBox();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.btnGuncelle = new System.Windows.Forms.Button();
+      this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.textBox5);
+      this.groupBox1.Controls.Add(this.textBox4);
+      this.groupBox1.Controls.Add(this.textBox3);
+      this.groupBox1.Controls.Add(this.textBox2);
+      this.groupBox1.Controls.Add(this.textBox1);
+      this.groupBox1.Controls.Add(this.label1);
+      this.groupBox1.Controls.Add(this.label5);
+      this.groupBox1.Controls.Add(this.label2);
+      this.groupBox1.Controls.Add(this.label4);
+      this.groupBox1.Controls.Add(this.label3);
+      this.groupBox1.Location = new System.Drawing.Point(9, 73);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(330, 285);
+      this.groupBox1.TabIndex = 4;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Görevli Bilgileri";
+      // 
+      // textBox5
+      // 
+      this.textBox5.Location = new System.Drawing.Point(103, 156);
+      this.textBox5.Name = "textBox5";
+      this.textBox5.Size = new System.Drawing.Size(160, 20);
+      this.textBox5.TabIndex = 4;
+      this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(103, 120);
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(100, 20);
+      this.textBox4.TabIndex = 3;
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(103, 87);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.Size = new System.Drawing.Size(100, 20);
+      this.textBox3.TabIndex = 2;
+      this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(103, 57);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(100, 20);
+      this.textBox2.TabIndex = 1;
+      this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Enabled = false;
+      this.textBox1.Location = new System.Drawing.Point(103, 25);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(16, 28);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(21, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "ID:";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(16, 161);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(39, 13);
+      this.label5.TabIndex = 1;
+      this.label5.Text = "Görev:";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(16, 60);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(25, 13);
+      this.label2.TabIndex = 0;
+      this.label2.Text = "Adı:";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(16, 123);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(32, 13);
+      this.label4.TabIndex = 0;
+      this.label4.Text = "Sınıfı:";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(16, 90);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(42, 13);
+      this.label3.TabIndex = 0;
+      this.label3.Text = "Soyadı:";
+      // 
+      // btnGuncelle
+      // 
+      this.btnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+      this.btnGuncelle.FlatAppearance.BorderSize = 0;
+      this.btnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnGuncelle.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+      this.btnGuncelle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+      this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnGuncelle.ImageKey = "edit.png";
+      this.btnGuncelle.ImageList = this.ımageList1;
+      this.btnGuncelle.Location = new System.Drawing.Point(212, 361);
+      this.btnGuncelle.Name = "btnGuncelle";
+      this.btnGuncelle.Size = new System.Drawing.Size(127, 37);
+      this.btnGuncelle.TabIndex = 0;
+      this.btnGuncelle.Text = "         Güncelle";
+      this.btnGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.btnGuncelle.UseVisualStyleBackColor = false;
+      this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click_1);
+      // 
+      // ımageList1
+      // 
+      this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+      this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+      this.ımageList1.Images.SetKeyName(0, "edit.png");
+      // 
+      // Kulüp_Yönetimi
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(348, 418);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.btnGuncelle);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.Name = "Kulüp_Yönetimi";
+      this.Resizable = false;
+      this.Style = MetroFramework.MetroColorStyle.Orange;
+      this.Text = "Kulüp Yönetimi";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.Button btnGuncelle;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.ImageList ımageList1;
+    private System.Windows.Forms.TextBox textBox5;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.TextBox textBox3;
+  }
+}
---
+++ MetroUI/kadroForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace MetroUI
+{
+  public partial class Kulüp_Yönetimi : MetroForm
+  {
+    public Kulüp_Yönetimi()
+    {
+      InitializeComponent();
+    }
+    SqlConnection connection = new SqlConnection("server=ACER-E1-571G\\SQLEXPRESS01;Database=KlupOtomasyon;Integrated Security=true");
+    private void btnGuncelle_Click_1(object sender, EventArgs e)
+    {
+      connection.Open();
+      SqlCommand guncelle = new SqlCommand("update Uyeler set uye_ad='" + textBox2.Text + "',uye_soyad='" + textBox3.Text + "',uye_sinif='" + textBox4.Text + "',uye_gorev='" + textBox5.Text + "' where uye_id='" + textBox1.Text + "'", connection);
+      guncelle.ExecuteNonQuery();
+      connection.Close();
+
+      MessageBox.Show("Kayıt Değiştirildi");
+      anasayfa k = Application.OpenForms["anasayfa"] as anasayfa;
+      k.kadroListele();
+      this.Close();
+    }
+
+    private void textBox2_Leave(object sender, EventArgs e)
+    {
+      textBox2.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(textBox2.Text);
+    }
+
+    private void textBox3_Leave(object sender, EventArgs e)
+    {
+      textBox3.Text=textBox3.Text.ToUpper();
+    }
+
+    private void textBox5_Leave(object sender, EventArgs e)
+    {
+      textBox5.Text = textBox5.Text.ToUpper();
+    }
+  }
+}
